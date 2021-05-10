f71a9ee20299e6b70ea821b1dabb9f65c178ffe6
Yoshiki Takamatsu <yoshi008t@gmail.com>
2021-05-10 23:19:47 +0900

    LINEメッセージ送信の実装

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"net/http"
-	"text/template"
-)
+import "github.com/mytysoldier/golang_linebot_messaging/func/router"
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	temp, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		panic(err.Error())
-	}
-	if err = temp.Execute(w, nil); err != nil {
-		panic(err.Error())
-	}
+	router.Router()
 }
